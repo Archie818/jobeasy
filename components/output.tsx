9379f795baf7479a5daf9130fdb533b13f44166f
Archie <archie.yang@dynamocover.com>
2023-03-20 11:47:54 +0000

    change output

--- components/output.tsx
+++ components/output.tsx
@@ -6,7 +6,7 @@ export default function Output({ result="" }) {
 			<div className="flex flex-col items-center justify-center mt-8">
 				
 				<div className="mt-4 text-center">
-					<h1 className="font-semibold text-gray-800 dark:text-white">{result}</h1>
+					<h1 className="font-semibold text-gray-800 text-white">{result}</h1>
 				</div>
 			</div>
 		)}
